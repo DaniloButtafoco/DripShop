bb9627d08aa75e835e788883eecbd10ace7d896e
Danilo <danilo.buttafoco@studet.kyh.se>
2021-05-26 16:26:15 +0200

    Final Product

---
+++ backend/data/products.js
@@ -0,0 +1,15 @@
+const products = [
+  {
+    name: 'Nike Air Force 1, White',
+    image: '/images/airpods.jpg', 
+    description: 'hello', 
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+]
+
+export default products
